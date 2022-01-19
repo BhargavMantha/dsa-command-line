909dbd7999268732ea07283f83a549f9ef8bbb6e
BhargavMantha <manthabhargav@gmail.com>
2022-01-19 11:00:27 +0530

    added the linked list module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IsNumberPrimeModule } from './is-number-prime/is-number-prime.module';
 import { MainMenuModule } from './main-menu/main-menu.module';
+import { LinkedListModule } from './linked-list/linked-list.module';
 
 @Module({
-  imports: [IsNumberPrimeModule, MainMenuModule],
+  imports: [IsNumberPrimeModule, MainMenuModule, LinkedListModule],
   controllers: [AppController],
   providers: [AppService],
 })
