b3f07c9ce774a7ed7e16f9f72b4bf72d1926ba25
BhargavMantha <manthabhargav@gmail.com>
2022-01-20 22:29:02 +0530

    1st q for arrays curious case benjamain bulbs along with test cases

--- src/arrays/arrays.service.spec.ts
+++ src/arrays/arrays.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MainMenuService } from '../main-menu/main-menu.service';
+import { keys } from '../main-menu/main-menu.service.spec';
 import { ArraysService } from './arrays.service';
-
+import { CuriousCaseOfBenjaminBulbs } from './class/curious-case-of-benjamin-bulbs.class';
+const data = {
+  inputData: 6
+};
 describe('ArraysService', () => {
   let service: ArraysService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArraysService],
+      providers: [ArraysService]
     }).compile();
 
     service = module.get<ArraysService>(ArraysService);
@@ -14,5 +19,53 @@ describe('ArraysService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+});
+
+describe('test, curious-case-of-benjamin-bulbs', () => {
+  let service: ArraysService;
+  let mainMenuService: MainMenuService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ArraysService]
+    }).compile();
+
+    service = module.get<ArraysService>(ArraysService);
+    const moduleMainMenuService: TestingModule = await Test.createTestingModule(
+      {
+        providers: [MainMenuService]
+      }
+    ).compile();
+
+    mainMenuService =
+      moduleMainMenuService.get<MainMenuService>(MainMenuService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('take number of Bulbs as 6', async () => {
+    const enterInput = async () => {
+      mainMenuService.writeToRl(data.inputData.toString());
+      mainMenuService.writeToRl(keys.enter);
+    };
+    setTimeout(() => enterInput().then(), 5);
+    try {
+      const input = await mainMenuService.askQuestion(
+        'Please enter number of bulbs: '
+      );
+      mainMenuService.closeRl();
+      expect(input).toBe(data.inputData.toString());
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it('check against number of bulbs that will be on after the nth fluctuation in voltage', () => {
+    const curiousCaseOfBenjaminBulbs = new CuriousCaseOfBenjaminBulbs();
+    const result = curiousCaseOfBenjaminBulbs.bulbsOnAfterNthFluctuation(
+      data.inputData
+    );
+    expect(result).toStrictEqual([1, 4]);
   });
 });
---
+++ src/arrays/class/curious-case-of-benjamin-bulbs.class.ts
@@ -0,0 +1,41 @@
+import { keys } from 'src/main-menu/main-menu.service.spec';
+import { MainMenuService } from '../../main-menu/main-menu.service';
+
+/**
+ * TODO:1. You are given n number of bulbs. They are all switched off. A weird fluctuation in voltage hits the circuit n times. In the 1st fluctuation all bulbs are toggled, in the 2nd fluctuation every 2nd bulb is toggled, in the 3rd fluctuation every 3rd bulb is toggled and so on. You've to find which bulbs will be switched on after n fluctuations.
+ * TODO:2. Take as input a number n, representing the number of bulbs.
+ * TODO:3. Print all the bulbs that will be on after the nth fluctuation in voltage.
+ */
+export class CuriousCaseOfBenjaminBulbs {
+  mainMenuService: MainMenuService;
+  numberOfOnBulbs = [];
+  constructor() {
+    this.mainMenuService = new MainMenuService();
+  }
+  async takeNumberOfBulbsAsInput() {
+    try {
+      const input = await this.mainMenuService.askQuestion(
+        'Please enter the number of bulbs: '
+      );
+      this.mainMenuService.closeRl();
+      return input;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  bulbsOnAfterNthFluctuation(numberOfBulbs: number) {
+    const bulbs = [];
+    for (let i = 0; i < numberOfBulbs; i++) {
+      bulbs.push('OFF');
+    }
+    for (let i = 0; i < numberOfBulbs; i++) {
+      for (let j = 0 + i; j < numberOfBulbs; j = j + i + 1) {
+        if (bulbs[j] === 'OFF') bulbs[j] = 'ON';
+        else if (bulbs[j] === 'ON') bulbs[j] = 'OFF';
+      }
+    }
+    for (let i = 0; i < bulbs.length; i++)
+      if (bulbs[i] === 'ON') this.numberOfOnBulbs.push(i + 1);
+    return this.numberOfOnBulbs;
+  }
+}
