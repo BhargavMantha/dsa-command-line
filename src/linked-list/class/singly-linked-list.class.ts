26f1a6e6a18132c1e4e36b404c54ee3f2b49e500
BhargavMantha <manthabhargav@gmail.com>
2022-02-01 18:34:15 +0530

    push traverse and pop impl

--- src/linked-list/class/singly-linked-list.class.ts
+++ src/linked-list/class/singly-linked-list.class.ts
@@ -8,33 +8,76 @@ export class SinglyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  /**
+   * 1. create a temporary variable to the function which will hold the current pointer to the linked-list
+   * 2. initially the temp variable say tempPointer will be head
+   * 3. loop over the linkedList until the next pointer on the tempPointer is null
+   */
+  traverseLinkedList() {
+    let tempPointer = this.head;
+    while (tempPointer.next !== null) {
+      console.log('The value ion the current node is: ', tempPointer.value);
+      tempPointer = tempPointer.next;
+    }
+  }
+  /**
+   * 1. create a new node with value as the item and next as null
+   * 2. if both head and tail are null
+   * 3. initialize both head and tail to be the new node
+   * 4. in case head is not null
+   * 4.1 loop over the linked list until you find the node with next as null call it current
+   * 4.2 now point current.next to be the new node
+   * 4.3 and the tai toi be new Node as well
+   * 4.4 increment the length of the linked-kist
+   * 5. return the linked list
+   */
   push(value: any) {
     const node = new Node(value);
-    if (!this.head) {
+    if (this.head === null) {
       this.head = node;
       this.tail = node;
     } else {
-      this.tail.next = node;
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
       this.tail = node;
     }
-    this.length += 1;
+    this.length++;
     return this;
   }
+  /**
+   * 1. create a variable previous item
+   * 2. initialize the previous item with head
+   * 3. initialize the current item to previous.next
+   * 4. iterate over the linked list until the next element of the linked-list is null
+   * 5. set the tail to previous item
+   * 6. and current to null
+   * 7. decrement the length
+   * 8. incase of length equal to 0 set both head and tail as null
+   */
   pop() {
-    if (!this.head) return undefined;
-    let current: Node = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
+    if (this.head === null) {
+      console.log('No items to POP :(');
+      return;
+    }
+    let previous = this.head;
+    let current = previous.next;
+    while (current && current.next) {
+      previous = current;
       current = current.next;
     }
-    this.length -= 1;
-    this.tail = newTail;
-    this.tail.next = null;
+    previous.next = null;
+    this.tail = previous;
+    const poppedItem = current;
+    current = undefined;
+    this.length--;
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
-    return this;
+    return poppedItem;
   }
 }
