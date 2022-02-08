91d9db74ac77fea44de4435c6994fc7bc45a4cee
BhargavMantha <manthabhargav@gmail.com>
2022-02-08 11:02:24 +0530

    added uuid and module updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IsNumberPrimeModule } from './is-number-prime/is-number-prime.module';
 import { MainMenuModule } from './main-menu/main-menu.module';
 import { LinkedListModule } from './linked-list/linked-list.module';
 import { ArraysModule } from './arrays/arrays.module';
 import { QueueModule } from './queue/queue.module';
+import { TreeModule } from './tree/tree.module';
+import { BinaryTreeModule } from './binart-tree/binary-tree.module';
 
 @Module({
-  imports: [IsNumberPrimeModule, MainMenuModule, LinkedListModule, ArraysModule, QueueModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    IsNumberPrimeModule,
+    MainMenuModule,
+    LinkedListModule,
+    ArraysModule,
+    QueueModule,
+    TreeModule,
+    BinaryTreeModule
+  ]
 })
 export class AppModule {}
